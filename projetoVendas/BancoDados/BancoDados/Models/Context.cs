b9ec29419eec4dfbe5e6394d39c5af6e058fdd61
Candido Tominaga <candido1212@gmail.com>
2019-09-12 19:24:31 -0300

    C3 - Banco de Dados - V1

---
+++ projetoVendas/BancoDados/BancoDados/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDados.Models
+{
+    class Context : DbContext
+    {
+        //Nomear o arquivo do banco de dados
+        public Context() : base("DbProdutos") { }
+
+        //Definir as classes que vão tabelas no banco
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
