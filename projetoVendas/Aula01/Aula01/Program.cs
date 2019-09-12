13f99b2cbbb66253ad06b5706294d778629fc421
Candido Tominaga <candido1212@gmail.com>
2019-09-12 19:15:03 -0300

    Aula 01

---
+++ projetoVendas/Aula01/Aula01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EntradaSaida();
+            ConversaoTipos();
+            Console.WriteLine("\nAperte uma tecla para continuar...");
+            Console.ReadKey();
+        }
+        public static void EntradaSaida()
+        {
+            //Imprimir uma mensagem no Console
+            Console.WriteLine("Primeiro programa em C#");
+
+            Console.WriteLine("\nDigite o seu nome:");
+            //Leitura dos dados do teclado
+            string nome = Console.ReadLine();
+            Console.WriteLine("O nome digitado foi: " + nome);
+        }
+        public static void ConversaoTipos()
+        {
+            Console.WriteLine("Digite a sua idade:");
+            //Converter tipos primitivos
+            //int idade = int.Parse(Console.ReadLine());
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if(idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+            }
+        }
+    }
+}
---
+++ projetoVendas/Aula01/Aula01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aula01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aula01")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5e7e66b-9114-40a9-aae3-d04f6f3838fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
