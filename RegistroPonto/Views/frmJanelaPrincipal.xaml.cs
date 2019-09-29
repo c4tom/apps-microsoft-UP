f0e63a65669414460d656adebbeef19fceed802a
Candido Tominaga <candido1212@gmail.com>
2019-09-29 20:21:09 -0300

    menu

--- RegistroPonto/Views/frmJanelaPrincipal.xaml.cs
+++ RegistroPonto/Views/frmJanelaPrincipal.xaml.cs
@@ -11,17 +11,47 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
-namespace RegistroPonto.Views
-{
+namespace RegistroPonto.Views {
     /// <summary>
     /// Lógica interna para frmJanelaPrincipal.xaml
     /// </summary>
-    public partial class frmJanelaPrincipal : Window
-    {
-        public frmJanelaPrincipal()
+    public partial class frmJanelaPrincipal : Window {
+        public frmJanelaPrincipal () {
+            InitializeComponent ();
+        }
+        private void FecharJanelaPrincipal (object sender, System.ComponentModel.CancelEventArgs e)
         {
-            InitializeComponent();
+            if (MessageBox.Show ("Deseja fechar a janela?", "Controle de Ponto",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+        private void BtnClickRegistrarPonto (object sender, RoutedEventArgs e)
+        {
+            frmRegistroPonto frm = new frmRegistroPonto ();
+            frm.ShowDialog ();
+        }
+        private void MnClickCadastroUsuario (object sender, RoutedEventArgs e)
+        {
+            frmCadastroUsuario frm = new frmCadastroUsuario ();
+            frm.ShowDialog ();
+        }
+        private void MnClickSobre (object sender, RoutedEventArgs e)
+        {
+            new frmSobre ().ShowDialog ();
+        }
+        private void MnClickCadastroCargos (object sender, RoutedEventArgs e)
+        {
+            new frmCargo ().ShowDialog ();
+        }
+        private void MnClickSair (object sender, RoutedEventArgs e)
+        {
+            Close ();
+        }
+        private void MnClickRegistrarPonto (object sender, RoutedEventArgs e)
+        {
+            new frmRegistroPonto ().ShowDialog ();
         }
     }
 }
